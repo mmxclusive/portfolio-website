86406728b272f1e7ddcff709cea8602a5b4eecb8
Muhammed Adelodun <mmxclusive@yahoo.com>
2020-04-13 17:54:27 -0700

    added type effect and other contents

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".typing-effect",{
+    strings: ["Muhammed Adelodun","a Web Developer","a Professional Photographer", "a Visual Story Teller","a Business Man"],
+    smartBackspace:true,
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true,
+    loopCount:Infinity,
+    startDelay:1000
+})
