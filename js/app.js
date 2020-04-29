1eede50c1c1f4ae0659be3833395fc4ec9d78a4d
Muhammed Adelodun <mmxclusive@yahoo.com>
2020-04-28 17:43:31 -0700

    media queries added for different mobile devices

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-var typed = new Typed(".typing-effect",{
-    strings: ["Muhammed Adelodun","a Web Developer","a Professional Photographer", "a Visual Story Teller","a Business Man"],
-    smartBackspace:true,
-    typeSpeed:100,
-    backSpeed:100,
-    loop:true,
-    loopCount:Infinity,
-    startDelay:1000
-})
+$(document).ready(function(){
+
+    var typed = new Typed(".typing-effect",{
+        strings: ["Muhammed Adelodun","a Web Developer","a Professional Photographer", "a Visual Story Teller","a Business Man"],
+        smartBackspace:true,
+        typeSpeed:100,
+        backSpeed:100,
+        loop:true,
+        loopCount:Infinity,
+        startDelay:1000
+    });
+
+    if($(window).width() <= 376){
+        $('#contact-details').remove()
+      }
+
+  });
+
+
+
