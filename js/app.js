ac706976b3ccca3da4b8aeab1c47af0129508a8c
mmxclusive <mmxclusive@yahoo.com>
2020-05-11 22:31:39 -0400

    navbar collapse after selection fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   
 
+
+    // Typing effect 
     var typed = new Typed(".typing-effect",{
         strings: ["Muhammed Adelodun","a Web Developer","a Professional Photographer", "a Visual Story Teller","a Business Man"],
         smartBackspace:true,
@@ -11,10 +13,13 @@ $(document).ready(function(){
         startDelay:1000
     });
 
-    // if($(window).width() <= 415){
-    //     $('#contact-details').remove()
-    //   };
 
+
+    // Collapse Nav-bar after selection clicked
+
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
       
 
 
