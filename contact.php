331537c34bfc32c73c71ef3dfe40fd77ef75c1c9
Muhammed Adelodun <mmxclusive@yahoo.com>
2020-05-12 16:50:53 -0700

    PHP code changed and favicon added

--- contact.php
+++ contact.php
@@ -1,25 +1,22 @@
-<?php
-    if(isset($_POST['submit'])){
-        
-        // Declaring the variables
-        $name = $_POST['fullname'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
 
 
-        $mailTo = 'muhammed@mmxclusive.com';
-        $subject = 'Form Submission';
-        $message = "Name: ".$name."\n"."Email: ".$email."\n". "Wrote the following: "."\n\n".$message;
-        $headers = "From: ".$email;
+<?php 
 
-        if(mail($mailTo, $subject, $message, $headers)){
+if(isset($_POST['submit'])){
+    $to = "muhammed@mmxclusive.com"; // this is your Email address
+    $from = $_POST['e-mail']; // this is the sender's Email address
+    $full_name = $_POST['full_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $full_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
 
-            echo"<h1>Message Sent Successfully! Thank You"." ".$name.", You will hear back shortly!</h1>";
-        }
-        else{
-            echo"Something went wrong!";
-        }
-
-
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $full_name . ", you will hear back shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
+
 ?>
